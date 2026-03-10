282f3373f191a524ade8a2e3cb9d7068e1c8a741
Ravanelly Rizqi Pratama <116047987+WaxYourHair@users.noreply.github.com>
2026-03-10 20:05:20 +0700

    Delete Soft-lany directory

--- Soft-lany/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { LyricsDisplay } from '@/components/lyrics-display';
-
-export default function Home() {
-  const audioUrl = process.env.NEXT_PUBLIC_AUDIO_URL || '';
-
-  return <LyricsDisplay audioUrl={audioUrl} />;
-}
